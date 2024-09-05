8a74c2b65acc5ad39d2ce65aedc28fce9169b1f5
aureprod <aureprod0@gmail.com>
2024-09-05 12:38:19 +0000

    fix: logger

--- builder/builder/utils/logger.py
+++ builder/builder/utils/logger.py
@@ -13,21 +13,15 @@ def setup_logger(debug: bool = False) -> None:
     # Get logs folder
     logs_folder_path = Path(__file__).parent.parent.parent
 
-    # Create file handler which logs even debug messages
-    file_handler = logging.FileHandler(logs_folder_path / "builder.log")
-    file_handler.setLevel(logging.DEBUG)
-
     # Create console handler with a higher log level
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logger_level)
 
     # Create formatter and add it to the handlers
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
 
     # Add the handlers to the logger
-    LOGGER.addHandler(file_handler)
     LOGGER.addHandler(console_handler)
 
     # Test the logger with an error message
